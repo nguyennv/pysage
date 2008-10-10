5f7e4044f2f1a2f6bbc4dd62260223958d7e1673
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-10-10 04:22:19 +0000

    testing singleton with threads

---
+++ tests/test_singleton.py
@@ -0,0 +1,29 @@
+# test_singleton.py
+from pysage.util import *
+import unittest
+import thread
+import processing
+import time
+
+class TestClass(ThreadLocalSingleton):
+    pass
+
+def return_instance_id():
+    return id(TestClass.get_singleton())
+
+class TestSingleton(unittest.TestCase):
+    def test_same_thread(self):
+        return return_instance_id() == return_instance_id()
+    def test_singleton_threadlocal(self):
+        threads = []
+        def change_id():
+            threads.append(id(TestClass.get_singleton()))
+        
+        assert not threads
+        thread.start_new_thread(change_id, ())
+        time.sleep(1)
+        assert threads[0]
+        assert not threads[0] == id(TestClass.get_singleton())
+        
+        
+        
