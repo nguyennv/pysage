95011da5935d2b53e3c32a877e6a653fb0aaf458
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-10-14 04:44:11 +0000

    tests for adding remove groups

--- branches/with_ipc/tests/test_groups_process.py
+++ branches/with_ipc/tests/test_groups_process.py
@@ -1,5 +1,6 @@
 # test_groups_process.py
 from pysage.network import *
+import multiprocessing
 import unittest
 
 nmanager = NetworkManager.get_singleton()
@@ -8,12 +9,30 @@ class TestGroupsProcess(unittest.TestCase):
     def setUp(self):
         pass
     def tearDown(self):
-        pass
-    def test_setgroups(self):
-        nmanager.add_process_group('resource_loading')
         nmanager.clear_process_group()
         nmanager.reset()
-    def test_shutdown(self):
-        pass
+    def test_add_group_single(self):
+        nmanager.add_process_group('resource_loading')
+        assert len(nmanager.groups) == 1
+        assert len(multiprocessing.active_children()) == 1
+    def test_add_group_multi(self):
+        nmanager.add_process_group('a')
+        nmanager.add_process_group('b')
+        assert len(nmanager.groups) == 2
+        assert len(multiprocessing.active_children()) == 2
+        nmanager.clear_process_group()
+        assert len(nmanager.groups) == 0
+        assert len(multiprocessing.active_children()) == 0
+    def test_remove_group_individual(self):
+        nmanager.add_process_group('a')
+        nmanager.add_process_group('b')
+
+        assert len(nmanager.groups) == 2
+        assert len(multiprocessing.active_children()) == 2
+
+        nmanager.remove_process_group('a')
+
+        assert len(nmanager.groups) == 1
+        assert len(multiprocessing.active_children()) == 1
 
 
