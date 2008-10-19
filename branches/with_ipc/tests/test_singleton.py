59de4b038f38abe7b56885b356110487a581c518
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-10-19 18:03:34 +0000

    adding processlocalsingleton

--- branches/with_ipc/tests/test_singleton.py
+++ branches/with_ipc/tests/test_singleton.py
@@ -19,36 +19,40 @@ except ImportError:
 if not processing:
     raise Exception('pysage requires either python2.6 or the "processing" module')
 
-class TestClass(ThreadLocalSingleton):
+class TestProcessSingleton(ProcessLocalSingleton):
+    pass
+
+class TestThreadSingleton(ThreadLocalSingleton):
     pass
 
 def return_instance_id():
-    return id(TestClass.get_singleton())
+    return id(TestProcessSingleton.get_singleton())
 
 def proc_change_id(q):
-    q.put(id(TestClass.get_singleton()))
+    q.put(id(TestProcessSingleton.get_singleton()))
 
 class TestSingleton(unittest.TestCase):
     def tearDown(self):
-        TestClass._clear_singleton()
+        TestProcessSingleton._clear_singleton()
+        TestThreadSingleton._clear_singleton()
     def test_same_thread(self):
         return return_instance_id() == return_instance_id()
     def test_singleton_threadlocal(self):
         '''tests that each thread has its own manager'''
         threads = []
         def change_id():
-            sid = id(TestClass.get_singleton())
+            sid = id(TestThreadSingleton.get_singleton())
             print 'new thread "%s": %s' % (thread.get_ident(), sid)
             threads.append(sid)
         
         assert not threads
         thread.start_new_thread(change_id, ())
         
-        nid = id(TestClass.get_singleton())
+        nid = id(TestThreadSingleton.get_singleton())
         print 'existing thread "%s": %s' % (thread.get_ident(), nid)
         time.sleep(1)
         print 'list contains: %s' % threads[0]
-        assert not threads[0] == id(TestClass.get_singleton())
+        assert not threads[0] == id(TestThreadSingleton.get_singleton())
     def test_singleton_process(self):
         '''tests that all processes have their own manager, --> unnecessary'''
         queue = processing.Queue()
@@ -59,10 +63,10 @@ class TestSingleton(unittest.TestCase):
         p.join()
         
         assert not queue.empty()
-        assert not queue.get() == id(TestClass.get_singleton())
+        assert not queue.get() == id(TestProcessSingleton.get_singleton())
     def test_singleton_process_before_fork(self):
         '''tests that all processes have their own manager, --> unnecessary'''
-        s = TestClass.get_singleton()
+        s = TestProcessSingleton.get_singleton()
         queue = processing.Queue()
         assert queue.empty()
         
@@ -71,7 +75,7 @@ class TestSingleton(unittest.TestCase):
         p.join()
         
         assert not queue.empty()
-        assert not queue.get() == id(TestClass.get_singleton())
+        assert not queue.get() == id(TestProcessSingleton.get_singleton())
         
         
         
