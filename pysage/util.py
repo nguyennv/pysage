cd649057ef8ff54b53da223f3fa040f16eb35535
bigjhnny <bigjhnny@1fa98330-223e-0410-ac30-ad2a981e1070>
2008-10-21 15:56:12 +0000

    adding exception upon registering same packet type

--- pysage/util.py
+++ pysage/util.py
@@ -36,26 +36,6 @@ class ProcessLocalSingleton(object):
     def _clear_singleton(cls):
         cls.__it__ = None
     
-class ThreadLocalSingleton(object):
-    '''not fork safe'''
-    def __new__(cls, *args, **kwds):
-        return cls.get_singleton(*args, **kwds)
-    @classmethod
-    def get_singleton(cls, *args, **kwds):
-        pool = cls.__dict__.get("__singleton_pool__")
-        if not pool:
-            pool = cls.__singleton_pool__ = threading.local()
-            
-        if not getattr(pool, '__singleton__',None):
-            pool.__singleton__ = object.__new__(cls)
-            pool.__singleton__.init(*args, **kwds)
-        return pool.__singleton__
-    def init(self, *args, **kwds):
-        pass
-    @classmethod
-    def _clear_singleton(cls):
-        cls.__singleton_pool__ = None
-
 if sys.platform.startswith("win"):
     get_time = time.clock
 else:
